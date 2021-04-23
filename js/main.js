22b2f3bc2c95161c009a178a9935004b1093ebd5
minnuss <ponjevic.goran@hotmail.com>
2021-04-23 14:26:32 +0200

    fixed console errors and forbid more then one char input

--- js/main.js
+++ js/main.js
@@ -3,23 +3,34 @@ const codes = document.querySelectorAll('.code')
 // focus on first input
 codes[0].focus()
 
+// forbid entering more then one number in input
+codes.forEach(code => {
+    code.addEventListener('input', () => {
+        if (code.value.length > 1) {
+            code.value = ''
+        }
+    })
+})
+
 codes.forEach((code, idx) => {
     code.addEventListener('keydown', (e) => {
-        // forbid entering more then one number in input
-        if (code.value.length > 1) return
-
         if (e.key >= 0 && e.key <= 9) {
             // if number is pressed, put focus on next input
             setTimeout(() => {
-                codes[idx + 1].focus()
+                // eliminates error in console when using idx+1
+                if (code.nextElementSibling) {
+                    code.nextElementSibling.focus()
+                }
             }, 10);
         } else if (e.key === "Backspace") {
             // if user mistakes a code, and press backspace, move focu to previous input
             setTimeout(() => {
-                // delete the previous input
-                codes[idx - 1].value = ''
-                // move focus to previous input
-                codes[idx - 1].focus()
+                if (code.previousElementSibling) {
+                    // delete the previous input
+                    codes[idx - 1].value = ''
+                    // move focus to previous input
+                    codes[idx - 1].focus()
+                }
             }, 10);
         }
     })
